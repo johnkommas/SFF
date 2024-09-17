b07cd69ff0e25b36278b488b28d4604d878de845
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2024-09-17 10:48:31 +0300

    Add docstrings to modal functions in modals.py
    Added detailed docstrings to functions in `dev_slack/modals.py` for better code documentation. Each function's purpose, parameters, and return values are now clearly described, providing better understanding and maintainability of the code.

--- dev_slack/modals.py
+++ dev_slack/modals.py
@@ -3,9 +3,17 @@ from dev_slack import functions
 
 def handle_archive_step_b(view):
     """
-    Διαβάζει τις ημερομηνίες έναρξης και Λήξης
-    :param view:
-    :return: None
+        Extracts the selected archive date from the view.
+
+        This function reads the data from a Slack view and extracts the selected start and end dates
+        from the 'archive_step_b' component.
+
+        Parameters:
+        view (dict): A dictionary representing the state of the Slack view from which the dates are
+                     to be extracted.
+
+        Returns:
+        str: The value of the selected option in 'archive_step_b' component.
     """
 
     key = view['state'].get('values').keys()
@@ -15,6 +23,17 @@ def handle_archive_step_b(view):
 
 
 def choose_archive():
+    """
+    Creates a modal view for archive search.
+
+    This function generates a modal view in Slack that allows users to choose from a list of requests.
+    The requests are loaded into the options for a static select menu, and the selected request's key
+    is returned when the user submits the form.
+
+    Returns:
+    dict: A dictionary representing a Slack modal view with a static select menu of requests.
+    """
+
     my_options = []
     requests = functions.load_requests()
     for key, value in requests.items():
@@ -76,6 +95,21 @@ def choose_archive():
 
 
 def represent_data(key):
+    """
+    Creates a modal to represent search results.
+
+    Given a key, this function retrieves a collection of requests, then generates
+    a representation of the status of the request associated with that key. The generated
+    status is used to create a Slack modal that presents the results to the user.
+
+    Parameters:
+    key (str): The key representing the request to find in the loaded requests.
+
+    Returns:
+    dict: A dictionary representing a Slack modal with content containing the data
+          from the selected request.
+    """
+
     requests = functions.load_requests()
     data = functions.display_status(requests, key)
 
@@ -97,6 +131,16 @@ def represent_data(key):
 
 
 def send_phones():
+    """
+    Creates a modal with contact phone numbers.
+
+    This function creates a Slack modal that shows an image and displays a list of
+    phone numbers from a text file 'phones.txt'.
+
+    Returns:
+    dict: A dictionary representing a Slack modal with an image block and section block containing phone numbers.
+    """
+
     image = 'filarmoniki.png'
 
     # Read the text from a .txt file
@@ -104,18 +148,18 @@ def send_phones():
         text = file.read()
 
     return {
-            "type": "modal",
-            "title": {"type": "plain_text", "text": "ΤΗΛΕΦΩΝΑ", "emoji": True},
-            "blocks": [
-                {
+        "type": "modal",
+        "title": {"type": "plain_text", "text": "ΤΗΛΕΦΩΝΑ", "emoji": True},
+        "blocks": [
+            {
                 "type": "image",
                 "image_url": f"https://raw.githubusercontent.com/johnkommas/CodeCademy_Projects/master/img/{image}",
                 "alt_text": "name",
             },
 
-                {
-                    "type": "section",
-                    "text": {"type": "mrkdwn", "text": text},
-                }
-            ],
-        }
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": text},
+            }
+        ],
+    }
